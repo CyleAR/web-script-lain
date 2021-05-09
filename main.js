a4d991c7af584954531817dad279eced6be4c1d5
cylear <cail15@naver.com>
2021-05-09 14:39:24 +0900

    add webfont

--- main.js
+++ main.js
@@ -6,8 +6,12 @@
 // @author       Cyle
 // @match        https://3d.laingame.net/
 // @icon         https://www.google.com/s2/favicons?domain=laingame.net
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
+
+GM_addStyle ( "@import url(http://fonts.googleapis.com/earlyaccess/nanumgothic.css);" );
+GM_addStyle ( "#subtitle { font-family: Nanum Gothic }" );
+GM_addStyle ( "#subtitle { font-size: 28px !important; }" );
 
 function replaceVTT()
 {
@@ -16,8 +20,8 @@ function replaceVTT()
     const trackPrefix = trackName.replace(/[0-9]/g,'');
     let newVTT = 'https://raw.githubusercontent.com/CyleAR/web-script-lain/master/Translations/' + trackPrefix + '/' + trackName + '.vtt';
     document.querySelector("#track").src = newVTT;
-    document.querySelector("#subtitle").style.fontFamily = "nanum gothic";
-    document.querySelector("#subtitle").style.fontSize = '28px';
+    //document.querySelector("#subtitle").style.fontFamily = "nanum gothic";
+    //document.querySelector("#subtitle").style.fontSize = '28px';
     return;
 }
 
