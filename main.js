b406d13d063754fe7b99cfd2f3e67773b99472c0
cylear <cail15@naver.com>
2021-05-09 14:23:12 +0900

    0.94

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+// ==UserScript==
+// @name         LainTSX Korean Subtitle
+// @namespace    http://cyle.xyz
+// @version      0.94
+// @description  Make me sad. Make me mad. Make me feel alright?
+// @author       Cyle
+// @match        https://3d.laingame.net/
+// @icon         https://www.google.com/s2/favicons?domain=laingame.net
+// @grant        none
+// ==/UserScript==
+
 function replaceVTT()
-{   
+{
     document.querySelector('#media').crossOrigin = 'anonymous';
     const trackName = document.querySelector('#track').src.substr(37,).split(`.`)[0]
     const trackPrefix = trackName.replace(/[0-9]/g,'');
@@ -7,7 +18,7 @@ function replaceVTT()
     document.querySelector("#track").src = newVTT;
     document.querySelector("#subtitle").style.fontFamily = "nanum gothic";
     document.querySelector("#subtitle").style.fontSize = '28px';
-    
+    return;
 }
 
 function waitTrackSrc()
@@ -19,9 +30,14 @@ function waitTrackSrc()
             if(!target.src == ''){
                 clearInterval(waitTrack);
                 replaceVTT();
+                return;
             }
         }catch{clearInterval(waitTrack)}
     }, 100);
+}
+
+function loadCSS(){
+
 }
 
 
@@ -33,6 +49,7 @@ function waitTrackSrc()
             if(!target){
                 clearInterval(scanInterval);
                 start();
+                return;
             }
         },100 );
     }
@@ -45,8 +62,11 @@ function waitTrackSrc()
                 clearInterval(startScanInterval);
                 scanner();
                 waitTrackSrc(target);
+                return;
             }
         },100)
     }
+
+    loadCSS();
     start();
 })();
