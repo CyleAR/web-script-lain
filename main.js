79ae76f7092eeb799e7fc695e90638c4fa0a2376
cylear <cail15@naver.com>
2021-05-09 15:20:32 +0900

    http -> https

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LainTSX Korean Subtitle
 // @namespace    http://cyle.xyz
-// @version      0.97
+// @version      0.98
 // @description  Make me sad. Make me mad. Make me feel alright?
 // @author       Cyle
 // @match        https://3d.laingame.net/
