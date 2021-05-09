ad2ea213d7bf99921093f76d8e70d45bfd5ec63d
cylear <cail15@naver.com>
2021-05-09 13:48:39 +0900

    almost done

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
 function replaceVTT()
 {   
-    try{
-        document.querySelector('#media').crossOrigin = 'anonymous';
-        const trackName = document.querySelector('#track').src.substr(37,).split(`.`)[0]
-        const trackPrefix = trackName.replace(/[0-9]/g,'');
-        let newVTT = 'https://raw.githubusercontent.com/CyleAR/web-script-lain/master/Translations/' + trackPrefix + '/' + trackName + '.vtt';
-        document.querySelector("#track").src = newVTT;
-        document.querySelector("#subtitle").style.fontFamily = "nanum gothic";
-        document.querySelector("#subtitle").style.fontSize = '28px';
-    }catch{}
+    document.querySelector('#media').crossOrigin = 'anonymous';
+    const trackName = document.querySelector('#track').src.substr(37,).split(`.`)[0]
+    const trackPrefix = trackName.replace(/[0-9]/g,'');
+    let newVTT = 'https://raw.githubusercontent.com/CyleAR/web-script-lain/master/Translations/' + trackPrefix + '/' + trackName + '.vtt';
+    document.querySelector("#track").src = newVTT;
+    document.querySelector("#subtitle").style.fontFamily = "nanum gothic";
+    document.querySelector("#subtitle").style.fontSize = '28px';
+    
 }
 
-function waitTrackSrc(target)
+function waitTrackSrc()
 {
+    //console.log('F waittracksrc started')
     let waitTrack = setInterval(() => {
-        if(!target.src == ''){
-            clearInterval(waitTrack);
-            replaceVTT();
-        }
+        let target = document.querySelector('#track');
+        try{
+            if(!target.src == ''){
+                clearInterval(waitTrack);
+                replaceVTT();
+            }
+        }catch{clearInterval(waitTrack)}
     }, 100);
 }
 
 
 (function() {
-    function scanner(target){
+    function scanner(){
+        //console.log("F scanner Started");
         let scanInterval = setInterval(() => {
+            let target = document.querySelector('#track');
             if(!target){
                 clearInterval(scanInterval);
                 start();
@@ -33,11 +38,12 @@ function waitTrackSrc(target)
     }
 
     function start(){
+        //console.log("F start Started");
         let startScanInterval = setInterval(() => {
             let target = document.querySelector('#track');
             if(target){
                 clearInterval(startScanInterval);
-                scanner(target);
+                scanner();
                 waitTrackSrc(target);
             }
         },100)
