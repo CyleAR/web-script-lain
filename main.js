ae6e2b2ef12e0fcda8958fb04b7e99e5c7d59a61
cylear <cail15@naver.com>
2021-05-14 23:51:18 +0900

    version up

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Web Script Lain
 // @namespace    http://cyle.xyz
-// @version      1.06
+// @version      1.07
 // @description  Make me sad. Make me mad. Make me feel alright?
 // @author       Cyle
 // @match        https://3d.laingame.net/
