f6a4f8d89327cda80c1a26e66d8996f6489446dc
Cyle <cail15@naver.com>
2021-08-04 22:00:46 +0900

    아하!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Web Script Lain
 // @namespace    http://cyle.xyz
-// @version      1.21
+// @version      1.22
 // @description  Multilangual support userscript for LainTSX
 // @author       Cyle
 // @match        https://3d.laingame.net/
